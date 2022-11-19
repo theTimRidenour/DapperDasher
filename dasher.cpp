201268a1765284f30451a5bd18cbef6715858bf5
Tim Ridenour <theTimRidenour@gmail.com>
2022-11-18 19:14:28 -0600

    combined nebulas into nebulae, gave santa db jmp

--- dasher.cpp
+++ dasher.cpp
@@ -11,7 +11,7 @@ struct AnimData
 
 struct player
 {
-    Texture2D name;
+    Texture2D charSheet;
     AnimData data;
 };
 
@@ -24,37 +24,34 @@ int main()
     int velocity = 0;
 
     // textures
-        // santa
-        Texture2D santa = LoadTexture("textures/santa.png");
-        AnimData santaData {
-            {0.0, 0.0, (float)santa.width/11, (float)santa.height/2}, // rec
-            {(float)((WINDOW_DIMENSIONS[0]/2) - (santa.width/22)), (float)(WINDOW_DIMENSIONS[1] - (santa.height/2))}, // pos
+        player players[2];
+        players[0].charSheet = {LoadTexture("textures/scarfy.png")};
+        players[0].data = {
+            {0.0, 0.0, (float)players[0].charSheet.width/6, (float)players[0].charSheet.height}, // rec
+            {(float)((WINDOW_DIMENSIONS[0]/2) - (players[0].charSheet.width/12)), (float)(WINDOW_DIMENSIONS[1] - players[0].charSheet.height)}, // pos
             0, 1.0/12.0, 0.0 // frame, updateTime, runningTime
         };
 
-        // scarfy
-        Texture2D scarfy = LoadTexture("textures/scarfy.png");
-        AnimData scarfyData {
-            {0.0, 0.0, (float)scarfy.width/6, (float)scarfy.height}, // rec
-            {(float)((WINDOW_DIMENSIONS[0]/2) - (scarfy.width/12)), (float)(WINDOW_DIMENSIONS[1] - scarfy.height)}, // pos
+        players[1].charSheet = {LoadTexture("textures/santa.png")};
+        players[1].data = {
+            {0.0, 0.0, (float)players[1].charSheet.width/11, (float)players[1].charSheet.height/2}, // rec
+            {(float)((WINDOW_DIMENSIONS[0]/2) - (players[1].charSheet.width/22)), (float)(WINDOW_DIMENSIONS[1] - (players[1].charSheet.height/2))}, // pos
             0, 1.0/12.0, 0.0 // frame, updateTime, runningTime
         };
 
-        player players[2]{{scarfy, scarfyData}, {santa, santaData}};
-
-        // nedula 1
+        // nedula texture
         Texture2D nebula = LoadTexture("textures/12_nebula_spritesheet.png");
-        AnimData neb1Data {
+
+        // nebula template data
+        AnimData nebDataTemp {
             {0.0, 0.0, (float)nebula.width/8, (float)nebula.height/8}, // rec
             {(float)WINDOW_DIMENSIONS[0], (float)(WINDOW_DIMENSIONS[1] - (nebula.height/8))}, // pos
             0, 1.0/12.0, 0.0 // frame, updateTime, runningTime
         };
 
-        // nebula 2
-        AnimData neb2Data {
-            neb1Data.rec, {(float)WINDOW_DIMENSIONS[0] + 300, neb1Data.pos.y}, // rec, pos
-            0, 1.0/16.0, 0.0 // frame, updateTime, runningTime
-        };
+        AnimData nebulae[2]{nebDataTemp, nebDataTemp};
+            nebulae[1].pos.x += 300;
+            nebulae[1].updateTime = 1.0/16.0;
 
     // nebula X velocity (pixels/second)
     int nebVel{-200};
@@ -63,7 +60,7 @@ int main()
     const int gravity = 1'250; // meters per second
     const int jumpVel = -600;
     bool jumping = false;
-    // bool doubleJump = false;
+    bool doubleJump = false;
 
     // active player (0 = scarfy, 1 = santa)
     int activePlayer{0};
@@ -81,8 +78,7 @@ int main()
         ClearBackground(WHITE);
         const float dT{ GetFrameTime() };
         players[activePlayer].data.runningTime += dT;
-        neb1Data.runningTime += dT;
-        neb2Data.runningTime += dT;
+        for (int i = 0; i < 2; i++) { nebulae[i].runningTime += dT; }
 
         // update animation frames for characters
         if (players[activePlayer].data.runningTime >= players[activePlayer].data.updateTime) {
@@ -109,81 +105,45 @@ int main()
             players[activePlayer].data.runningTime = 0.0;
         }
 
-        // update animation frames from nebula 1
-        if (neb1Data.runningTime >= neb1Data.updateTime) {
-            if (neb1Data.frame >= 56) {
-                neb1Data.rec.y = 7 * neb1Data.rec.height;
-                neb1Data.rec.x = (neb1Data.frame - 56) * neb1Data.rec.width;
-            } else if (neb1Data.frame >= 48) {
-                neb1Data.rec.y = 6 * neb1Data.rec.height;
-                neb1Data.rec.x = (neb1Data.frame - 48) * neb1Data.rec.width;
-            } else if (neb1Data.frame >= 40) {
-                neb1Data.rec.y = 5 * neb1Data.rec.height;
-                neb1Data.rec.x = (neb1Data.frame - 40) * neb1Data.rec.width;
-            } else if (neb1Data.frame >= 32) {
-                neb1Data.rec.y = 4 * neb1Data.rec.height;
-                neb1Data.rec.x = (neb1Data.frame - 32) * neb1Data.rec.width;
-            } else if (neb1Data.frame >= 24) {
-                neb1Data.rec.y = 3 * neb1Data.rec.height;
-                neb1Data.rec.x = (neb1Data.frame - 24) * neb1Data.rec.width;
-            } else if (neb1Data.frame >= 16) {
-                neb1Data.rec.y = 2 * neb1Data.rec.height;
-                neb1Data.rec.x = (neb1Data.frame - 16) * neb1Data.rec.width;
-            } else if (neb1Data.frame >= 8) {
-                neb1Data.rec.y = neb1Data.rec.height;
-                neb1Data.rec.x = (neb1Data.frame - 8) * neb1Data.rec.width;
-            } else {
-                neb1Data.rec.y = 0;
-                neb1Data.rec.x = neb1Data.frame * neb1Data.rec.width;
+        // update animation frames for nebulae
+        for (int i = 0; i < 2; i++) {
+            if (nebulae[i].runningTime >= nebulae[i].updateTime) {
+                if (nebulae[i].frame >= 56) {
+                    nebulae[i].rec.y = 7 * nebulae[i].rec.height;
+                    nebulae[i].rec.x = (nebulae[i].frame - 56) * nebulae[i].rec.width;
+                } else if (nebulae[i].frame >= 48) {
+                    nebulae[i].rec.y = 6 * nebulae[i].rec.height;
+                    nebulae[i].rec.x = (nebulae[i].frame - 48) * nebulae[i].rec.width;
+                } else if (nebulae[i].frame >= 40) {
+                    nebulae[i].rec.y = 5 * nebulae[i].rec.height;
+                    nebulae[i].rec.x = (nebulae[i].frame - 40) * nebulae[i].rec.width;
+                } else if (nebulae[i].frame >= 32) {
+                    nebulae[i].rec.y = 4 * nebulae[i].rec.height;
+                    nebulae[i].rec.x = (nebulae[i].frame - 32) * nebulae[i].rec.width;
+                } else if (nebulae[i].frame >= 24) {
+                    nebulae[i].rec.y = 3 * nebulae[i].rec.height;
+                    nebulae[i].rec.x = (nebulae[i].frame - 24) * nebulae[i].rec.width;
+                } else if (nebulae[i].frame >= 16) {
+                    nebulae[i].rec.y = 2 * nebulae[i].rec.height;
+                    nebulae[i].rec.x = (nebulae[i].frame - 16) * nebulae[i].rec.width;
+                } else if (nebulae[i].frame >= 8) {
+                    nebulae[i].rec.y = nebulae[i].rec.height;
+                    nebulae[i].rec.x = (nebulae[i].frame - 8) * nebulae[i].rec.width;
+                } else {
+                    nebulae[i].rec.y = 0;
+                    nebulae[i].rec.x = nebulae[i].frame * nebulae[i].rec.width;
+                }
+
+                // frame update for nebulae
+                nebulae[i].frame++;
+                if (nebulae[i].frame > 60) { nebulae[i].frame = 0; }
+                nebulae[i].runningTime = 0.0;
             }
 
-            // frame update for nebula
-            neb1Data.frame++;
-            if (neb1Data.frame > 60) { neb1Data.frame = 0; }
-            neb1Data.runningTime = 0.0;
+            // update nebulae position
+            nebulae[i].pos.x += nebVel *dT;
+            if (nebulae[i].pos.x < -nebulae[i].rec.width) { nebulae[i].pos.x = WINDOW_DIMENSIONS[0]; }
         }
-
-        // update animation frames from nebula 2
-        if (neb2Data.runningTime >= neb2Data.updateTime) {
-            if (neb2Data.frame >= 56) {
-                neb2Data.rec.y = 7 * neb2Data.rec.height;
-                neb2Data.rec.x = (neb2Data.frame - 56) * neb2Data.rec.width;
-            } else if (neb2Data.frame >= 48) {
-                neb2Data.rec.y = 6 * neb2Data.rec.height;
-                neb2Data.rec.x = (neb2Data.frame - 48) * neb2Data.rec.width;
-            } else if (neb2Data.frame >= 40) {
-                neb2Data.rec.y = 5 * neb2Data.rec.height;
-                neb2Data.rec.x = (neb2Data.frame - 40) * neb2Data.rec.width;
-            } else if (neb2Data.frame >= 32) {
-                neb2Data.rec.y = 4 * neb2Data.rec.height;
-                neb2Data.rec.x = (neb2Data.frame - 32) * neb2Data.rec.width;
-            } else if (neb2Data.frame >= 24) {
-                neb2Data.rec.y = 3 * neb2Data.rec.height;
-                neb2Data.rec.x = (neb2Data.frame - 24) * neb2Data.rec.width;
-            } else if (neb2Data.frame >= 16) {
-                neb2Data.rec.y = 2 * neb2Data.rec.height;
-                neb2Data.rec.x = (neb2Data.frame - 16) * neb2Data.rec.width;
-            } else if (neb2Data.frame >= 8) {
-                neb2Data.rec.y = neb2Data.rec.height;
-                neb2Data.rec.x = (neb2Data.frame - 8) * neb2Data.rec.width;
-            } else {
-                neb2Data.rec.y = 0;
-                neb2Data.rec.x = neb2Data.frame * neb2Data.rec.width;
-            }
-
-            // frame update for nebula 2
-            neb2Data.frame++;
-            if (neb2Data.frame > 60) { neb2Data.frame = 0; }
-            neb2Data.runningTime = 0.0;
-        }
-
-        // update nebula 1 position
-        neb1Data.pos.x += nebVel * dT;
-        if (neb1Data.pos.x < -neb1Data.rec.width) { neb1Data.pos.x = WINDOW_DIMENSIONS[0]; }
-
-        // update nebula 2 position
-        neb2Data.pos.x += nebVel * dT;
-        if(neb2Data.pos.x < -neb2Data.rec.width) { neb2Data.pos.x = WINDOW_DIMENSIONS[0]; }
 
         // update player's position
         players[activePlayer].data.pos.y += velocity * dT;
@@ -194,13 +154,15 @@ int main()
                 velocity = 0;
                 players[activePlayer].data.pos.y = WINDOW_DIMENSIONS[1] - players[activePlayer].data.rec.height;
                 jumping = false;
-                // doubleJump = false;
+                doubleJump = false;
             } else {
                 velocity += gravity * dT;
-                /* if (IsKeyPressed(KEY_SPACE) && !doubleJump) {
-                    velocity = jumpVel;
-                    doubleJump = true;
-                } */
+                if (activePlayer == 1) {
+                    if (IsKeyPressed(KEY_SPACE) && !doubleJump) {
+                        velocity = jumpVel;
+                        doubleJump = true;
+                    }
+                }
             }
         } else {
             if (IsKeyPressed(KEY_SPACE)) {
@@ -210,21 +172,17 @@ int main()
         }
 
         // draw characters
-        DrawTextureRec(players[activePlayer].name,
+        DrawTextureRec(players[activePlayer].charSheet,
                        players[activePlayer].data.rec, 
                        players[activePlayer].data.pos, 
                        WHITE);
         
-        // draw nebulas
-        DrawTextureRec(nebula, neb1Data.rec, neb1Data.pos, WHITE);
-        DrawTextureRec(nebula, neb2Data.rec, neb2Data.pos, RED);
-
+        // draw nebulae
+        for (int i = 0; i < 2; i++) { DrawTextureRec(nebula, nebulae[i].rec, nebulae[i].pos, WHITE); }
+        
         EndDrawing();
     }
-    UnloadTexture(scarfy);
-    UnloadTexture(santa);
-    UnloadTexture(players[0].name);
-    UnloadTexture(players[1].name);
+    for (int i=0; i < 2; i++) { UnloadTexture(players[i].charSheet); }
     UnloadTexture(nebula);
     CloseWindow();
 }
